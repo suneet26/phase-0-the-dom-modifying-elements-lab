bc3611ced92e50bca1b6b1b2cf0bbe8fa445cdee
Sunee Thoedpraiphanawan <amythoedpranawan@yahoo.com>
2022-05-21 12:25:56 -0600

    commit tree 9ded31a919b743d5751cfbeef9b8a8527f122567

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+//create your new element here and assign it to newHeader
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Sunee is the champion!";
+newHeader.className = "victory";
+
+//enter the code to remove the node element here
+document.querySelector("main#main").remove();
+
+// main.remove();
+// const newHeader = document.createElement("h1");
+// newHeader.setAttribute("id", "victory");
+// newHeader.innerHTML ="Sunee is the champion";
+// document.body.append(newHeader);
